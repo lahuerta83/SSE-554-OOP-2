1635673269672cc0474a862591b032e6ccc2cd56
lahuerta83 <Leigh.A.Huerta@live.mercer.edu>
2017-03-12 21:51:54 -0400

    Project 3 code

---
+++ RockyGroomingApp/dbo.Dog.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Dog] (
+    [ID]          INT            IDENTITY (1, 1) NOT NULL,
+    [Appointment] DATETIME2 (7)  NOT NULL,
+    [Groomer]     NVARCHAR (MAX) NULL,
+    [Name]        NVARCHAR (MAX) NULL,
+    [Service]     NVARCHAR (MAX) NULL,
+    CONSTRAINT [PK_Dog] PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
